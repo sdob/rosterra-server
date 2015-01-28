329e9c7efb8d4f8f2c3ddeb89bea685024abbb53
sdob <sdob@rosterra.com>
2015-01-28 18:35:35 +0000

    /authenticate endpoint is now handled by custom token authentication

--- rosterra/urls.py
+++ rosterra/urls.py
@@ -1,11 +1,8 @@
 from django.conf.urls import patterns, include, url
-from rest_framework.authtoken import views as tokenviews
-
-#from django.contrib import admin
-#admin.autodiscover()
+from custom_auth import views as tokenviews
 
 urlpatterns = patterns('',
-        # /authenticate/ s routed to the token authentication service
+        # /authenticate/ is routed to our slight modification of the token authentication service
         url(r'^authenticate', tokenviews.obtain_auth_token, name='token-auth'),
         # everything else is routed to our main API app
         url(r'^', include('main.urls')),
